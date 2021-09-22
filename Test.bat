d268620195576219b10843bc0508de841f7233cf
Jacob-Relle <hp176@uni-heidelberg.de>
2021-09-22 16:06:47 +0200

    standard test

---
+++ Test.bat
@@ -0,0 +1,8 @@
+cd ..
+cd Documents
+cd Python_Scripts
+call C:\Users\Hernienforschung\anaconda3\Scripts\activate.bat C:\Users\Hernienforschung\anaconda3
+call activate biomedisa
+cd hernia-repair
+call python Einlesungs_script.py
+pause
