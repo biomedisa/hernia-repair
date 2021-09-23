8fc34b3f24093f20e026dacb5a79328f480d593f
Jacob-Relle <hp176@uni-heidelberg.de>
2021-09-23 18:49:13 +0200

    standard test

--- Einlesungs_script.py
+++ Einlesungs_script.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import os
+#from Prediction import hernien_höhe
+from paraview.simple import *
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 import pydicom
 import glob
@@ -8,11 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import shutil
 import tkinter as tk
+
 from tkinter.filedialog import askdirectory
 from tifffile import imread
 from subprocess import call
-from paraview.simple import *
 from datetime import datetime
+from PIL import Image
 
 def paraview_screenshot(mesh):
     Connect()
@@ -82,7 +85,6 @@ def paraview_screenshot(mesh):
     #Reset the session
     Disconnect()
 
-
 def Creat_CT_crosssection(path_to_tif,path_to_dcm):
     #get tif with labels
     label = imread(path_to_tif)
@@ -101,6 +103,34 @@ def Creat_CT_crosssection(path_to_tif,path_to_dcm):
     path_to_png = path_to_dcm + '_crosssection.png'
     plt.imsave(path_to_png,img,cmap='gray')
     return path_to_png
+
+def fill_height_dir(path_to_data,save_path):
+    #get the dcm files
+    slices = glob.glob(path_to_data+'/**/*', recursive=True)
+    #set start values
+    ds = pydicom.filereader.dcmread(slices[0])
+    img = ds.pixel_array
+    #initalize array for the data
+    data = np.zeros((len(slices), ds.Rows, ds.Columns), dtype=img.dtype)
+    #loop over all files
+    for file in slices:
+        #get slice information
+        ds = pydicom.filereader.dcmread(file)
+        img = ds.pixel_array
+        if len(img.shape) == 3 and img.shape[2] == 1:
+            img = img[:,:,0]
+        #set corresponding slice in the array
+        data[ds.InstanceNumber-1] = img
+    #convert to uint8
+    data = np.uint8(data*255./np.amax(data))
+    #creat subfolder
+    if not os.path.exists(save_path+'\\Daten'):
+        os.mkdir(save_path+'\\Daten')
+    #save every layer
+    for z in range(data.shape[0]):
+        ## save data as tif
+        img = Image.fromarray(data[z,:,:], mode='L')
+        img.save(save_path +'\\Daten\\'+ str(z).zfill(6) +'.tif')
 
 
 if __name__ == "__main__":
@@ -127,25 +157,34 @@ if __name__ == "__main__":
                 #read dicom properties        
                 ds = pydicom.filereader.dcmread(file)
                 #name the directory containing all results after patient name + Birthdate     	      
-                first_level = main_path +'\\'+str(ds.PatientName)+'_'+str(ds.PatientBirthDate)  
-                #Set the subdirectorys
+                first_level = main_path +'\\'+str(ds.PatientName)+'_'+str(ds.PatientBirthDate)
+                first_level = first_level.replace('^','_')  
+                #Set the subdirectories
                 nativ_dir = first_level+'\\nativ'
+                nativ_height_dir = first_level+'\\nativ_height'
                 valsalva_dir = first_level+'\\valsalva'
+                valsalva_height_dir = first_level+'\\valsalva_height'
                 #Create the directorys if not existing            
                 if not os.path.exists(first_level):
                     os.mkdir(first_level)
                 if not os.path.exists(nativ_dir):
                     os.mkdir(nativ_dir)
+                if not os.path.exists(nativ_height_dir):
+                    os.mkdir(nativ_height_dir)
                 if not os.path.exists(valsalva_dir):					
-                    os.mkdir(valsalva_dir)             
+                    os.mkdir(valsalva_dir)
+                if not os.path.exists(valsalva_height_dir):
+                    os.mkdir(valsalva_height_dir)             
                 #Store String of the Series Name 
                 series = str(ds.SeriesDescription)
+                #Check for the Bvalue
                 if 'B20s' in series:
                     Bvalue = 'B20s'                    
                     B20s = True
                 elif 'B31s' in series:
                     Bvalue = 'B31s'
-                 
+    
+    #fill the subdirectories         
     for file in files:      
         if os.path.isfile(file):
             #read dicom properties  
@@ -180,18 +219,21 @@ if __name__ == "__main__":
 
     #Execute Neural Net for nativ
     if nat_exists:
+        #fill the height directory with 
+        fill_height_dir(nativ_dir,nativ_height_dir)
+        #nativ_hernia_height = hernien_höhe(r'C:\Users\Hernienforschung\Documents\Python_Scripts\Hernien_detector_z.h5',nativ_height_dir)
         net1 = call(["python",r"C:\Users\Hernienforschung\git\biomedisa\demo\biomedisa_deeplearning.py", 
                     nativ_dir, r"C:\Users\Hernienforschung\Documents\Python_Scripts\img_hernie.h5", "-p","-bs","6"]
                     )
+        #Create Paths to the mesh and the img
         nativ_tif = first_level+'\\final.nativ.tif'
+        nativ_vtk = nativ_tif.replace('.tif','.vtk')
+        nativ_png = nativ_tif.replace('.tif','.png')
 
         #Create Nativ mesh in vtk format for Paraview
         mesh1 = call(["python",r"C:\Users\Hernienforschung\Documents\Python_Scripts\create_mesh.py", 
                     nativ_tif, slice_thickness]
                     )
-        #Create Paths to the mesh and the img
-        nativ_vtk = nativ_tif.replace('.tif','.vtk')
-        nativ_png = nativ_tif.replace('.tif','.png')
 
         #Create Image using Paraview
         paraview_screenshot(nativ_vtk)
@@ -202,19 +244,20 @@ if __name__ == "__main__":
 
     #Execute Neural Net for valsalva
     if val_exists:
+        fill_height_dir(valsalva_dir,valsalva_height_dir)
+        #valsalva_hernia_height = hernien_höhe(r'C:\Users\Hernienforschung\Documents\Python_Scripts\Hernien_detector_z.h5',valsalva_height_dir)
         net2 = call(["python",r"C:\Users\Hernienforschung\git\biomedisa\demo\biomedisa_deeplearning.py",
                     valsalva_dir, r"C:\Users\Hernienforschung\Documents\Python_Scripts\img_hernie.h5", "-p","-bs","6"]
                     )
+        #Create Paths to the mesh and the img                    
         valsalva_tif = first_level+'\\final.valsalva.tif'
-    	
+        valsalva_vtk = valsalva_tif.replace('.tif','.vtk')
+        valsalva_png = valsalva_tif.replace('.tif','.png')   
+
         #Create Valsalva mesh in vtk format for Paraview
         mesh2 = call(["python",r"C:\Users\Hernienforschung\Documents\Python_Scripts\create_mesh.py", 
                     valsalva_tif, slice_thickness]
                     )
-
-        #Create Paths to the mesh and the img
-        valsalva_vtk = valsalva_tif.replace('.tif','.vtk')
-        valsalva_png = valsalva_tif.replace('.tif','.png')
 
         #Create img using Paraview
         paraview_screenshot(valsalva_vtk)
@@ -268,4 +311,3 @@ if __name__ == "__main__":
     #if either scan is missing do nothing
     else:
         print('Error: Nativ or Valsalva Data missing')
-    
