fbd2669d0c5aedae32370c47c02299aa7851dbc8
Philipp Lösel <philipp.loesel@uni-heidelberg.de>
2022-01-31 17:44:41 +0100

    unnecessary files removed

--- Git_download.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-cd Documents
-cd Python_Scripts
-call C:\Users\Hernienforschung\anaconda3\Scripts\activate.bat C:\Users\Hernienforschung\anaconda3
-call activate biomedisa
-cd hernia-repair
-
-git checkout main
-git pull git@github.com:biomedisa/hernia-repair.git
