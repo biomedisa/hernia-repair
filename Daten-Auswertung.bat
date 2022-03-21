62274cb47edff9c21552bf5962a85178f8325c6f
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-03-22 00:11:34 +0100

    removed hardcoded paths

--- Daten-Auswertung.bat
+++ Daten-Auswertung.bat
@@ -1,18 +1,14 @@
-cd ..
-cd Documents
-cd Python_Scripts
-call C:\Users\Hernienforschung\anaconda3\Scripts\activate.bat C:\Users\Hernienforschung\anaconda3
-call activate biomedisa
-cd hernia-repair
-
 git checkout main
 git pull
 
-cd..
+cd ..
+
+call \anaconda3\Scripts\activate.bat \anaconda3
+call activate biomedisa
+
 rmdir /s /Q Temp
 mkdir Temp
-cd Temp
 
-call python C:\Users\Hernienforschung\Documents\Python_Scripts\hernia-repair\Einlesungs_script.py
+call python \hernia-repair\Einlesungs_script.py
 
 pause
