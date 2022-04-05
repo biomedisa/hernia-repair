f2bb51cbbfdbcdb4fcb867579f2e63132ce03a18
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-04-05 15:03:16 +0200

    Update to execution
    Update to be consise with file structure

--- Daten-Auswertung.bat
+++ Daten-Auswertung.bat
@@ -1,14 +1,25 @@
-git checkout main
+git checkout 21.03
 git pull
 
 cd ..
 
-call \anaconda3\Scripts\activate.bat \anaconda3
+cd biomedisa
+git checkout main
+git pull
+cd ..
+cd ..
+
+
+call anaconda3\Scripts\activate.bat \anaconda3
 call activate biomedisa
+
+cd git
 
 rmdir /s /Q Temp
 mkdir Temp
 
 call python \hernia-repair\Einlesungs_script.py
+
+rmdir /s /Q Temp
 
 pause
