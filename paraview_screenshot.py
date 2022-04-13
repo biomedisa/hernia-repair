878abdd64c4364e3845f8b5f934f39d1fe03b267
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-04-13 19:01:25 +0200

    Update paraview_screenshot.py

--- paraview_screenshot.py
+++ paraview_screenshot.py
@@ -26,13 +26,14 @@ renderView1 = GetActiveViewOrCreate('RenderView')
 ParaviewDisplay = Show(Paraview, renderView1, 'GeometryRepresentation')
 
 # get color transfer function/color map for 'scalars'
-scalarsLUT = GetColorTransferFunction('scalars')
-scalarsLUT.RescaleTransferFunction(1.0, 7.0)
+#scalarsLUT = GetColorTransferFunction('scalars')
+#scalarsLUT.RescaleTransferFunction(1.0, 7.0)
 
 # trace defaults for the display properties.
+ParaviewDisplay.RescaleTransferFunctionToDataRange(False,True)
 ParaviewDisplay.Representation = 'Surface'
 ParaviewDisplay.ColorArrayName = ['CELLS', 'scalars']
-ParaviewDisplay.LookupTable = scalarsLUT
+#ParaviewDisplay.LookupTable = scalarsLUT
 ParaviewDisplay.SelectTCoordArray = 'None'
 ParaviewDisplay.SelectNormalArray = 'None'
 ParaviewDisplay.SelectTangentArray = 'None'
@@ -51,7 +52,7 @@ ParaviewDisplay.DataAxesGrid = 'GridAxesRepresentation'
 ParaviewDisplay.PolarAxes = 'PolarAxesRepresentation'
 
 # hide color bar/color legend
-ParaviewDisplay.SetScalarBarVisibility(renderView1, False)
+#ParaviewDisplay.SetScalarBarVisibility(renderView1, False)
 
 # Hide orientation axes
 renderView1.OrientationAxesVisibility = 0
