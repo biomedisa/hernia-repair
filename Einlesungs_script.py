99140f2f58e87d16e42057633512c99b3e93deb9
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-04-14 11:20:39 +0200

    Update Einlesungs_script.py

--- Einlesungs_script.py
+++ Einlesungs_script.py
@@ -170,9 +170,11 @@ def create_numpy_layer(path_to_data):
     file = open(path_to_data,mode='r')
     data_string = file.read()
     data_list = data_string.split("\n")
+    print(len(data_list),data_list[-1])
     vector_array = [data_list[i].split(',') for i in range(len(data_list))]
-    vector_array = np.array(vector_array, dtype=float)
     print(vector_array[-1])
+    vector_array = np.array(vector_array, dtype=float)
+
     print(vector_array.shape)
     data_array = np.sqrt(vector_array[:,:vector_array.shape[1]/2]**2 + vector_array[:,vector_array.shape[1]/2:]**2)
     print(data_array)
