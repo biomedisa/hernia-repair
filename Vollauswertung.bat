71ec3d936616312e54346a8e810d25e02b1474ad
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-04-23 14:57:32 +0200

    Update Vollauswertung.bat

--- Vollauswertung.bat
+++ Vollauswertung.bat
@@ -2,18 +2,21 @@ git checkout main
 git pull
 
 cd ..
+
 cd biomedisa
 git checkout master
 git pull
-cd..
+cd ..
+
+
+call "%userprofile%\anaconda3\Scripts\activate.bat"
+call activate biomedisa
 
 rmdir /s /Q Temp
 mkdir Temp
+
 cd Temp
 
-call %userprofile%\anaconda3\Scripts\activate.bat
-call activate biomedisa
-
-call python C:\Users\Hernienforschung\Documents\Python_Scripts\hernia-repair\Einlesungs_script.py Multi
+call python "%Userprofile%\git\hernia-repair\Einlesungs_script.py" Multi
 
 pause
