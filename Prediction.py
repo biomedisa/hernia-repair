9e54f9748029bf34fda1957d74728488e5bf55ec
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-04-27 19:12:50 +0200

    Add files via upload

--- Prediction.py
+++ Prediction.py
@@ -1,11 +1,10 @@
 import numpy as np
-import pydicom, os, glob, sys
+import pydicom, glob, sys, os
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 from tifffile import imread
 
-def fill_length_dir(path_to_data,save_path):
+def load_data(path_to_data):
     #get the dcm files
     slices = glob.glob(path_to_data+'/**/*', recursive=True)
     #set start values
@@ -13,6 +12,7 @@ def fill_length_dir(path_to_data,save_path):
     img = ds.pixel_array
     #initalize array for the data
     data = np.zeros((len(slices), ds.Rows, ds.Columns), dtype=img.dtype)
+    header = [0] * len(slices)
     #loop over all files
     for file in slices:
         #get slice information
@@ -22,51 +22,29 @@ def fill_length_dir(path_to_data,save_path):
             img = img[:,:,0]
         #set corresponding slice in the array
         data[ds.InstanceNumber-1] = img
-    #convert to uint8
+        header[ds.InstanceNumber-1] = ds
+    #convert to uint8 and make rgb
     data = np.uint8(data*255./np.amax(data))
-    #creat subfolders
-    if not os.path.exists(save_path+'\\Länge'):
-        os.mkdir(save_path+'\\Länge')
-        os.mkdir(save_path+'\\Länge\\Daten')
-    if not os.path.exists(save_path+'\\Breite'):
-        os.mkdir(save_path+'\\Breite')
-        os.mkdir(save_path+'\\Breite\\Daten')
-    #save every layer in z and x direction
-        for z in range(data.shape[0]):
-            ## save data as tif
-            img = Image.fromarray(data[z,:,:], mode='L')
-            img.resize((512,512))
-            img.save(save_path +'\\Länge\\Daten\\'+ str(z).zfill(6) +'.tif')
+    data = np.stack((data,)*3,axis=-1)
 
-        for x in range(data.shape[2]):
-            ## save data as tif
-            img = Image.fromarray(data[:,:,x], mode='L')
-            img.resize((512,512))
-            img.save(save_path +'\\Breite\\Daten\\'+ str(x).zfill(6) +'.tif')
+    #for width detection
+    '''
+    for x in range(data.shape[2]):
+        ## save data as tif
+        img = Image.fromarray(data[:,:,x], mode='L')
+        img.resize((512,512))
+        img.save(save_path +'\\Breite\\Daten\\'+ str(x).zfill(6) +'.tif')
+    '''
+    return data,header
 
-def get_hernia_length(path_to_model,path_to_data):
+def get_hernia_length(path_to_model,data):
 
     model = tf.keras.models.load_model(path_to_model)
 
-    datagen = ImageDataGenerator( 
-    )
+    prediction = model.predict(data, batch_size = 32,verbose = 0)
 
-    hernia_data = datagen.flow_from_directory(
-        path_to_data,
-        class_mode= None,
-        color_mode='rgb',
-        batch_size= 32,
-        shuffle= False,
-        target_size=(512,512),
-    )
-
-    prediction = model.predict(hernia_data, verbose=0)
-
-    hernia_interval = (prediction > 0.5).flatnonzero()
-    try:
-        hernia_length = hernia_interval[-1] - hernia_interval[0]
-    except:
-        hernia_length = -1
+    hernia_interval = (prediction > 0.5).nonzero()[0]
+    hernia_length = hernia_interval[-1] - hernia_interval[0]
 
     return hernia_length
 
@@ -81,7 +59,7 @@ def annotate_by_label(path_to_tif,path_to_dcm):
     zsh, _, _ = a.shape
 
     # load tomographic data & get pixel spacing
-    file = path_to_dcm + '\\' + str(1).zfill(6) + '.dcm'
+    file = f'{path_to_dcm}/{str(1).zfill(6)}.dcm'
 
     # get slice thickness
     ds = pydicom.filereader.dcmread(file)
@@ -111,52 +89,43 @@ def annotate_by_label(path_to_tif,path_to_dcm):
             hernia_area += np.sqrt((x_res*(x_max-x_min))**2) * z_res
     return area, hernia_area
 
-def annotate_by_neural_net(path_to_dcm,path_to_length_dir):
-    #fill the length directory with images
-    fill_length_dir(path_to_dcm,path_to_length_dir)
-    #Create Paths to both subdirectories
-    height_dir = path_to_length_dir + '\\Länge'
-    width_dir  = path_to_length_dir + '\\Breite'
+def annotate_by_neural_net(path_to_dcm):
     #Get slice width 
-    file = path_to_dcm + '\\' + str(1).zfill(6) + '.dcm'
-    ds = pydicom.filereader.dcmread(file)
-    slice_thickness = ds.SliceThickness
-    _ , slice_width = ds.PixelSpacing
+    data, header = load_data(path_to_dcm)
+    slice_thickness = header[0].SliceThickness
+    _ , slice_width = header[0].PixelSpacing
     #Get Height,width and area of the hernia
-    hernia_width = get_hernia_length(r"C:\Users\Hernienforschung\Documents\Python_Scripts\Netzwerke\Hernien_detector_x.h5",width_dir)
-    hernia_width *= slice_width*0.1
-    hernia_height = get_hernia_length(r"C:\Users\Hernienforschung\Documents\Python_Scripts\Netzwerke\Hernien_detector_z.h5",height_dir)
+    #hernia_width = get_hernia_length(f'{os.environ["userprofile"]}\\git\\Netzwerke\\z_dense_fine.h5',np.swapaxes(data,0,2))
+    #hernia_width *= slice_width*0.1
+    hernia_width = 0.
+    hernia_height = get_hernia_length(f'{os.environ["userprofile"]}\\git\\Netzwerke\\z_dense_fine.h5' ,data)
     hernia_height *= slice_thickness*0.1
     hernia_area = get_hernia_area(hernia_height,hernia_width)
     return hernia_width, hernia_height ,hernia_area
 
-def annotate_image(observation,path_to_dcm,path_to_length_dir,path_to_tif,path_to_png):
-    hernia_width_by_nn, hernia_height_by_nn,hernia_area_by_nn = annotate_by_neural_net(path_to_dcm,path_to_length_dir)
+def annotate_image(observation,path_to_dcm,path_to_tif,path_to_png):
+    hernia_width_by_nn, hernia_height_by_nn,hernia_area_by_nn = annotate_by_neural_net(path_to_dcm)
     instable_area_by_label, hernia_area_by_label = annotate_by_label(path_to_tif,path_to_dcm)
     
     #write hernia dimensions on the image
     to_annotate = Image.open(path_to_png)
     draw = ImageDraw.Draw(to_annotate)
-    font = ImageFont.truetype("arial.ttf",size=20)
-    draw.text(xy=(to_annotate.width/2,0),
+    draw.text(xy=(0,0),
             text= (f'{observation}\n'
                    f'(Berechnete Größen) Breite: {str(round(hernia_width_by_nn,1))}cm   Länge: {str(round(hernia_height_by_nn,1))}cm    Bruchpforten Fläche: {str(round(hernia_area_by_nn,1))}cm²\n'
                    f'(Größen im Bild) Instabile_Fläche: {str(round(instable_area_by_label*0.01,1))}cm²   Projezierte Fläche: {str(round(hernia_area_by_label*0.01,1))}cm²'),     
             fill=(0,0,0),
-            anchor="ma",
-            font=font,
+            align ="center"
             )
     to_annotate.save(path_to_png,format='png')
 
 
 observation = sys.argv[1]
 path_to_dcm = sys.argv[2]
-path_to_length_dir = sys.argv[3]
-path_to_tif = sys.argv[4]
-path_to_png = sys.argv[5]
+path_to_tif = sys.argv[3]
+path_to_png = sys.argv[4]
 annotate_image(observation,
                 path_to_dcm,
-                path_to_length_dir,
                 path_to_tif,
                 path_to_png
                 )
