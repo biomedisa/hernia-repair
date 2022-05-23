3b321eb316804d76355a4d86cdf02f4086dea714
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-05-23 17:31:58 +0200

    Update Einlesungs_script.py

--- Einlesungs_script.py
+++ Einlesungs_script.py
@@ -302,11 +302,11 @@ def get_distortion_dim(path_to_tif,slice_thickness, x_dim):
     zsh, _, _ = img.shape
     height_array = np.any(img >= 15, axis =(1,2))
     if np.any(height_array):
-        height = (np.flatnonzero(height_array)[-1] - np.flatnonzero(height_array)[0]) * int(slice_thickness) * 0.1
+        height = (np.flatnonzero(height_array)[-1] - np.flatnonzero(height_array)[0]) * float(slice_thickness) * 0.1
     else: height = 0
     width_array = np.any(img >= 15, axis =(0,1))
     if np.any(width_array):
-        width = (np.flatnonzero(width_array)[-1] - np.flatnonzero(width_array)[0]) * int(x_dim) * 0.1
+        width = (np.flatnonzero(width_array)[-1] - np.flatnonzero(width_array)[0]) * float(x_dim) * 0.1
     else: width = 0
     area_array = np.any(img >= 15, axis=1)
     if area_array.size != 0:
