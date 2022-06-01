2a95c24c4357d99b7dc79fb7d897f4bf28b7d6b2
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2022-06-01 17:13:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 path_names = {
-    'main': f'{os.environ["userprofile"]}\\Hernien_Analyse_{mode}',
+    'main': f'{os.environ["userprofile"]}\\Hernien_Analyse_Single',
     'neuralnet': f'{os.environ["userprofile"]}\\git\\Netzwerke',
     'multipath': f'{os.environ["userprofile"]}\\git\\Pfade.txt'
 }
