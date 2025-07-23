90236be417e4e3a2d8ac43220d6753fac730fdf7
Philipp Lösel <70630836+philipp-loesel@users.noreply.github.com>
2025-07-23 18:04:25 +1000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 call "%Userprofile%\anaconda3\Scripts\activate.bat"
-call conda env create -f environment.yml
+call conda env create -f "%Userprofile%\git\hernia-repair\environment.yml"
 call activate biomedisa
 pause
 
